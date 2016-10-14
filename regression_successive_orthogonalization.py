c4b08614e3e68e8d20b4912f3df44ed687bc8cf3
minwoobae <minubae.nyc@gmail.com>
2016-10-14 15:02:14 -0400

    updated the algorithm 3.1.

--- regression_successive_orthogonalization.py
+++ regression_successive_orthogonalization.py
@@ -1,8 +1,10 @@
 import numpy as np
-#def regression_successive_orthogonalization(X):
+#def regression_successive_orthogonalization(X, Y):
 
 X = np.array(np.mat([[1, 2, 3, 4], [3, 4, 4, 5], [4, 5, 7, 6]]))
+Y = np.array(np.mat([[30, 32, 23, 14], [53, 24, 34, 45], [34, 15, 37, 46]]))
 X_T = X.transpose()
+X_T = T.transpose()
 shape = np.shape(X_T)
 p = shape[0]
 m = shape[1]
@@ -32,15 +34,16 @@ print(X_T)
 Q = [[0 for i in range(p)] for j in range(p-1)]
 print('Coefficient: ', Q)
 for j in range(p):
-    print(j)
-    print(X_T[j])
-    l = j-1
-    Q[l][j] = np.inner(Z[l], X_T[j]) / np.inner(Z[l], Z[l])
-    Z[j] = X_T[j] - Q[l][j]*Z[l].sum()
-    print('Q: ', Q)
-    print('Z: ', Z[j])
+    for l in range(p-1):
+        print(j)
+        print(X_T[j])
+        Q[l][j] = np.inner(Z[l], X_T[j]) / np.inner(Z[l], Z[l])
+        Z[j] = X_T[j] - Q[l][j]*Z[l].sum()
+        print('Q: ', Q)
+        print('Z: ', Z[j])
 
-estimate_beta = 0
-estimate_beta = np.inner(Z[p], y) / np.inner(Z[p], Z[p])
+estimate_beta = list()
+for i in range(p):
+    estimate_beta[i] = np.inner(Z[i], Y_T[i]) / np.inner(Z[i], Z[i])
 
-return estimate_beta
+#print(estimate_beta)
