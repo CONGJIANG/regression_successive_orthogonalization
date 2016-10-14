88a19239f92db1ddb585a50440f2fc74e33d1ed2
minwoobae <minubae.nyc@gmail.com>
2016-10-14 14:54:11 -0400

    updated algorithm 3.1.

--- regression_successive_orthogonalization.py
+++ regression_successive_orthogonalization.py
@@ -2,9 +2,10 @@ import numpy as np
 #def regression_successive_orthogonalization(X):
 
 X = np.array(np.mat([[1, 2, 3, 4], [3, 4, 4, 5], [4, 5, 7, 6]]))
-X_T = X.transpose(); shape = np.shape(X_T); p = shape[0]; m = shape[1]
-
-#print('shape: ', shape); print('vector_01: ', X[0,:])
+X_T = X.transpose()
+shape = np.shape(X_T)
+p = shape[0]
+m = shape[1]
 
 print('matrix X: ')
 print(X)
@@ -12,40 +13,34 @@ print(X)
 # Initialize x_0 and z_0 with 0's elements which is the vector 1.
 x_0 = np.ones((1, m), dtype=int)
 z_0 = np.ones((1, m), dtype=int) #z_0 = np.array(np.full((1, shape[1]), 1, dtype=int))
-
-
 X_T = np.insert(x_0, 1, X_T, 0)
+
+Z = np.empty([p,m], dtype=int)
+Z = np.insert(z_0, 1, Z, 0)
+print('Z: ')
+print(Z)
+
+shape = np.shape(X_T)
+p = shape[0]
 
 print('matrix X_T: ')
 print(X_T)
-print('dimension: ',p)
-print('num_elements: ',m)
+#print('shape: ', shape);
+#print('dimension: ', p)
+#print('num_elements: ',m)
 
-for x in X_T:
-    print(x)
+Q = [[0 for i in range(p)] for j in range(p-1)]
+print('Coefficient: ', Q)
+for j in range(p):
+    print(j)
+    print(X_T[j])
+    l = j-1
+    Q[l][j] = np.inner(Z[l], X_T[j]) / np.inner(Z[l], Z[l])
+    Z[j] = X_T[j] - Q[l][j]*Z[l].sum()
+    print('Q: ', Q)
+    print('Z: ', Z[j])
 
+estimate_beta = 0
+estimate_beta = np.inner(Z[p], y) / np.inner(Z[p], Z[p])
 
-#for i in range(p):
-#    if i == 0:
-#        Z = np.array(np.full((1, shape[1]), 1, dtype=int))
-        #Z = np.ones(i, shape[1])
-        #x[i] = np.full((1, shape[1]), 1, dtype=int)
-        #print(z[i])
-        #print(x[i])
-#        print('hello everyone.', Z)
-    #else:
-        #x[i] =
-
-
-#    x[i] = X[i,:]
-#    print(x[i])
-
-
-#z_0 = np.array([p]) #x_0 = np.array([p]) #z_0.fill(1) #x_0.fill(1)
-
-#for i in range(1, p+1):
-    #np.inner(a, b)
-#    print(i)
-    #vector[i] = a[i, :]
-
-#    return X
+return estimate_beta
